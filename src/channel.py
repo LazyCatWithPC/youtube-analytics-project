234a6e420f786063d6ae043d3d7d38315380dd9c
LazyCatWithPC <i.treukhov@gmail.com>
2023-10-08 13:20:40 +0400

    updated channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+from googleapiclient.discovery import build
+import json
+import os
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    # Программа не видела переменную среды, так что пришлось добавить в PyCharm
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
