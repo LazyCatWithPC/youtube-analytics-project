7f6c16a889f1700708dc7bf3258a8fa9243d6d4f
LazyCatWithPC <i.treukhov@gmail.com>
2023-10-28 18:57:51 +0400

    updated video.py

--- src/video.py
+++ src/video.py
@@ -4,12 +4,20 @@ from googleapiclient.discovery import build
 
 class Video:
     def __init__(self, video_id: str):
-        self._video_id = video_id
-        self._youtube = self.get_service()
-        self.title = self._youtube['items'][0]['snippet']['title']
-        self.url = self._youtube['items'][0]['id']
-        self.view_count = self._youtube['items'][0]['statistics']['viewCount']
-        self.like_count = self._youtube['items'][0]['statistics']['likeCount']
+        try:
+            self._video_id = video_id
+            self._youtube = self.get_service()
+            self.title = self._youtube['items'][0]['snippet']['title']
+            self.url = self._youtube['items'][0]['id']
+            self.view_count = self._youtube['items'][0]['statistics']['viewCount']
+            self.like_count = self._youtube['items'][0]['statistics']['likeCount']
+            if not self._youtube:
+                raise IndexError(self._youtube)
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def get_service(self):
         youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
